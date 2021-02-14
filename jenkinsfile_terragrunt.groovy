b2d8dae1d78caf5bad27a07eb58d62f10e5f8aa8
vinitkapoor <vin_kapoor@yahoo.com>
2021-02-14 12:15:29 +0530

    none

--- jenkinsfile_terragrunt.groovy
+++ jenkinsfile_terragrunt.groovy
@@ -2,23 +2,29 @@ import org.yaml.snakeyaml.*
 import groovy.json.*
 
 def myname = "/Users/vinitkapoor"
+def products_dir = "products"
+def environment_dir = "environment"
 pipeline {
     agent any
     parameters{
-        string(defaultValue: 'vinit', description: '', name: 'myname', trim: false)
+        string(defaultValue: 'vinit', description: 'Product', name: 'myname', trim: false)
+        choice(choices: 'analytics\ncallstats', description: 'Products', name: 'products')
     }
     stages {
-        stage('Hello') {
+        stage('Preregs&Checkout') {
             steps {
-                sh "cd ${myname}; pwd"
-                sh "pwd"
-                dir("/Users/vinitkapoor/Learning/jenkins"){
-                    sh 'echo "this is a message"'
-                    sh 'pwd'
-                }
-                echo "My name is == ${params.myname}"
                 git branch: 'main', changelog: false, poll: false, url: 'https://github.com/vinitkapoor/jenkins.git'
             }
+        }
+
+        stage('Plan'){
+            sh "cd ${myname}; pwd"
+            sh "pwd"
+            dir("${params.products}"){
+                sh 'pwd'
+            }
+            echo "My name is == ${params.myname}"
+
         }
     }
 }
